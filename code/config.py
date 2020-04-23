a8a49801c20253af3d414862437746d1a2b96066
Dirk Roorda <dirk@annotation.nl>
2020-04-23 23:09:09 +0200

    streamlining for v8

--- code/config.py
+++ code/config.py
@@ -30,71 +30,36 @@ EXAMPLE_SECTION = "<code>P509373</code>"
 EXAMPLE_SECTION_TEXT = "P509373"
 
 DATA_DISPLAY = dict(
-    noneValues={None},
-    sectionSep1=" ",
-    sectionSep2=":",
     writing="cun",
-    writingDir="ltr",
-    fontName="Santakku",
-    font="Santakk.ttf",
-    fontw="Santakku.woff",
     textFormats={
         "layout-orig-rich": "layoutRich",
         "layout-orig-unicode": "layoutUnicode",
     },
-    browseNavLevel=2,
-    browseContentPretty=False,
 )
 
 TYPE_DISPLAY = dict(
     document=dict(
-        template="{pnumber}",
-        featuresBare="collection volume document docnote",
-        children="face",
-        lineNumber="srcLnNum",
-        level=3, flow="col", wrap=False, stretch=False,
+        featuresBare="collection volume document docnote", lineNumber="srcLnNum",
     ),
-    face=dict(
-        template="{face}",
-        featuresBare="object",
-        children="line",
-        lineNumber="srcLnNum",
-        level=3, flow="col", wrap=False, strectch=False,
-    ),
+    face=dict(featuresBare="object", lineNumber="srcLnNum",),
     line=dict(
-        template="{lnno}",
         features="remarks translation@en",
         children={"word", "commentline"},
-        condense=True,
         lineNumber="srcLnNum",
-        level=2, flow="row", wrap=True, strectch=True,
     ),
-    cluster=dict(
-        template="{type}",
-        children="sign",
-        level=2, flow="row", wrap=True, strectch=False,
-    ),
-    word=dict(
-        template=True,
-        children="sign",
-        base=True,
-        level=1, flow="row", wrap=False, strectch=True,
-    ),
+    cluster=dict(template="{type}", children="sign", stretch=False,),
+    word=dict(template=True, base=True, wrap=False,),
     sign=dict(
-        template=True,
         features=(
             "collated remarkable question damage"
             " det uncertain missing excised supplied langalt"
             " comment remarks"
             " repeat fraction operator grapheme"
         ),
-        level=0, flow="col", wrap=False, strectch=False,
     ),
 )
 
-INTERFACE_DEFAULTS = dict(
-    lineNumbers=False,
-)
+INTERFACE_DEFAULTS = dict(lineNumbers=False,)
 
 
 def deliver():
